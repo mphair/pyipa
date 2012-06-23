23b5396c189f28fe05658b343c6c5aff2ca6a23b
Mark E. Phair <mphair@gmail.com>
2012-06-22 17:59:36 -0700

    Load dictionary files * added classes to manage languages and language families * not yet hooked up to the ipaParser or the soundChange stuff

---
+++ dictionaryManager.py
@@ -0,0 +1,78 @@
+# -*- encoding: utf-8 -*-
+###
+# Manage on-disk dictionaries of words
+#
+
+from languageFamily import *
+
+BaseDictionaryHowTo = """
+The dictionary format is a tab-separated file in the following form per line:
+
+language<tab>word<tab>part of speech<tab># wikimedia markup of definition
+
+Markup includes:
+ * [[other word]]
+ * {{grammatical note}}, e.g. {{countable}} for nouns or {{transitive}} for verbs
+
+You can get a good starting point for natural lanauges from Wiktionary:
+REMEMBER THAT USING THAT DATA REQUIRES YOU TO FOLLOW THE FREE DOCUMENTATION LICENSE TERMS.
+PLEASE SEE THE SITE FOR DETAILS.
+
+http://en.wiktionary.org/wiki/Help:FAQ#Downloading_Wiktionary
+
+In particular, check out the "just definitions" files available at
+
+http://toolserver.org/~enwikt/definitions/
+
+which were the basis of the dictionary format used here.
+
+You probably want enwikt-defs-latest-en.tsv.gz. If you want non-english langauges, you'll have to split them out yourself.
+
+You can decompress that file using gzip from gzip.org.
+
+Dictionaries are organized by family. Create a series of nested folders appropriately.
+For example,
+
+pyipa
+    data
+        dictionaries
+            Indo-European
+                Germanic
+                    West Germanic
+                        Anglo-Frisian
+                            Anglic
+                                English   <-- dictionary file
+
+(see http://en.wikipedia.org/wiki/English_language for the above accepted family tree of English)
+"""
+
+def GetDictionariesPath():
+    import sys
+    import os
+    if hasattr(sys.modules['__main__'], "__file__"):
+        basePath = path.abspath(sys.modules['__main__'].__file__)
+    elif sys.argv[0] != '':
+        basePath = os.path.dirname(sys.argv[0])
+    else:
+        basePath = os.getcwd()
+    s = os.path.join(basePath, "data", "dictionaries")
+    if os.path.exists(s): return s
+    else: return ""
+
+def LoadFromDefault():
+    dataPath = GetDictionariesPath()
+    allFamilies = LanguageFamily("all families")
+    if dataPath != "":
+        allFamilies.LoadFromPath(dataPath)
+    return allFamilies                                                                                                                       
+
+if __name__ == '__main__':
+    print "Loading families..."
+    allFamilies = LoadFromDefault()
+    if len(allFamilies.AllChildLanguages().keys()) == 0:
+        print "*"*80
+        print "You don't seem to have any languages"
+        print "*"*80
+        print BaseDictionaryHowTo
+    else:
+        print allFamilies.FamilyTree("*", 0)
---
+++ languageFamily.py
@@ -0,0 +1,49 @@
+# -*- encoding: utf-8 -*-
+###
+## Classes to contain and maintain data about language families and the languages therein
+
+def ParseDictionaryFile(fileName):
+    f = open(fileName)
+    splitlines = [[l.strip() for l in line.split("\t")] for line in f.readlines()]
+    d = {}
+    for line in splitlines:
+        if not(d.has_key(line[1])): d[line[1]] = []
+        d[line[1]].append(line[2:])    
+    return d
+
+class Language:
+    def __init__(self, name, vocabulary):
+        self.Name = name
+        self.Vocabulary = vocabulary
+    def __repr__(self):
+        return self.Name + " (" + str(len(self.Vocabulary.keys())) + " words)"
+    def FamilyTree(self, indentStr, indentAmount):
+        return indentStr*indentAmount + str(self) + "\n"
+
+class LanguageFamily:
+    def __init__(self, name):
+        self.SubFamilies = []
+        self.Languages = {}
+        self.Name = name
+    def __repr__(self):
+        return self.Name + " (" + str(len(self.AllChildLanguages().keys())) + " languages)"
+    def LoadFromPath(self, path):
+        import os
+        for entry in os.listdir(path):
+            fullPath = os.path.join(path, entry)
+            if os.path.isdir(fullPath):
+                family = LanguageFamily(entry)
+                family.LoadFromPath(fullPath)
+                self.SubFamilies.append(family)
+            else:
+                self.Languages[entry] = Language(entry, ParseDictionaryFile(fullPath))
+    def AllChildLanguages(self):
+        result = dict(self.Languages)
+        for family in self.SubFamilies:
+            result = dict(result.items() + family.AllChildLanguages().items())            
+        return result
+    def FamilyTree(self, indentStr, indentAmount):
+        return (indentStr*indentAmount + str(self) + "\n"
+            + "".join([l.FamilyTree(indentStr, indentAmount+1) for l in self.Languages.values()])
+            + "".join([f.FamilyTree(indentStr, indentAmount+1) for f in self.SubFamilies]))
+
