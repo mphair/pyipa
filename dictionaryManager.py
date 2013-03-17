c403a5274969a78b3f426dae33cc1260d13b21bd
Mark E. Phair <mphair@gmail.com>
2013-03-16 23:56:33 -0700

    more western-specific stuff

--- dictionaryManager.py
+++ dictionaryManager.py
@@ -92,13 +92,27 @@ import pickle
 import os
 def loadwestern():
     global western
+    global genders
+    global tenses
     western = allFamilies.AllChildLanguages()['Western']
     os.chdir("..")
+    genders = [u'MALE',u'FEMALE',u'CHILD',u'AGED']
+    tenses = [u'distant past', u'memorable past', u'future', u'present']
+
+def loadimpiety():
+    global western
+    global genders
+    global tenses
+    impiety = allFamilies.AllChildLanguages()['Impiety']
+    os.chdir("..")
+    genders = [u'MALE',u'FEMALE',u'CHILD',u'AGED']
+    tenses = [u'distant past', u'memorable past', u'future', u'present']    
 
 def initials(actor): return '/'.join([a[0] for a in actor])
 
 def add_qualified_nouns():
-    ###### Add plurals before using this
+    #### WARNING: THIS ONLY WORKS IF THE ADJ IS THE FIRST ENTRY FOR THE WORD
+    #### WARNING: Add plurals before using this
     global definiteness
     global nouns
     global qual_nouns
@@ -106,7 +120,7 @@ def add_qualified_nouns():
     defins = definiteness.keys()
     nouns = [[item[0]]+item[1][0][1:] for item in western.Vocabulary.items() if item[1][0][0] == u'N']
     qual_nouns = [( CombineAffix(noun[0],definiteness[d][noun[2]]), noun[1]+u"("+d+u")", noun[2], noun[3], noun[4]) for noun in nouns for d in defins]
-    pickle.dump(qual_nouns, open('qual_nouns.pickle','w'))
+    ###>> pickle.dump(qual_nouns, open('qual_nouns.pickle','w'))
     for item in qual_nouns:
         word,d,g,s,l = item
         if not(word in western.Vocabulary):
@@ -114,11 +128,12 @@ def add_qualified_nouns():
         western.Vocabulary[word].append([u'QUALN',d,g,s,l])
 
 def add_conj_verbs():
+    #### WARNING: THIS ONLY WORKS IF THE V IS THE FIRST ENTRY FOR THE WORD
     global agreement
     global verbs
     global tense
     tense = pickle.load(open('tense.pickle'))
-    allattrib = [(g,s,l,t) for t in tense.keys() for g in [u'MALE',u'FEMALE',u'CHILD',u'AGED'] for s in [u'LOW',u'MEDIUM',u'HIGH'] for l in [u'LIGHT',u'NEUTRAL',u'DARK']]
+    allattrib = [(g,s,l,t) for t in tense.keys() for g in genders for s in [u'LOW',u'MEDIUM',u'HIGH'] for l in [u'LIGHT',u'NEUTRAL',u'DARK']]
     agreement = pickle.load(open('agreement.pickle'))
     verbs = [[item[0]]+item[1][0][1:] for item in western.Vocabulary.items() if item[1][0][0] == u'V']
     conj_verbs = [( CombineAffix(CombineAffix(verb[0],vagree(verb[2:],attri[:3])),tense[attri[3]][verb[2]]), verb[1]+u"(actor="+initials(attri[:3])+u","+attri[3]+u")", attri[0], attri[1], attri[2]) for verb in verbs for attri in allattrib]
@@ -129,6 +144,7 @@ def add_conj_verbs():
             western.Vocabulary[word] = []
         western.Vocabulary[word].append([u'CONJV',d,g,s,l])
 def add_conj_adj():
+    #### WARNING: THIS ONLY WORKS IF THE ADJ IS THE FIRST ENTRY FOR THE WORD
     global adj_agreement
     global adjectives
     global conj_adj
@@ -142,6 +158,56 @@ def add_conj_adj():
         if not(word in western.Vocabulary):
             western.Vocabulary[word] = []
         western.Vocabulary[word].append([u'CONJADJ',d,l])
+
+def add_pn():
+    global pns
+    pns = pickle.load(open('pns.pickle'))
+    for word,d,g in [[pns[gender][x],u'# PN,'+gender+u','+x,gender] for gender in genders for x in [u'SINGULAR',u'PLURAL']]:
+        if not(word in western.Vocabulary):
+            western.Vocabulary[word] = []
+        western.Vocabulary[word].append([u'PN',d,g])    
+
+def produce_impiety():
+    global impiety
+    loadwestern()
+    allsc = allFamilies.AllAvailableSoundChanges()
+    sc = allsc['Western-Impiety']
+    impiety = Language.FromSoundChange(western, "Impiety", soundChange.SoundChange.FromSoundChangeList(sc).Apply)
+
+def flattenVocab(vocab):
+    result = []
+    for item in vocab.items():
+        result.extend([[item[0]]+entry for entry in item[1]])
+    return result
+
+def produceGroupedVerbs(vocab):
+    global fv
+    global verbs
+    global conjv
+    global grouped_v
+    fv = flattenVocab(vocab)
+    verbs = [v for v in fv if v[1] == u'V']
+    conjv = [cv for cv in fv if cv[1] == u'CONJV']
+    grouped_v = {v[0]:(v[1:],[cv for cv in conjv if cv[2].startswith(v[2]) ]) for v in verbs}
+
+def getTenseFromGv(gv,ii): # gv is an item from grouped_v above, ii is the index of the conjv inside the given gv
+    return gv[1][1][ii][2][len(gv[1][0][1])+13:-1]
+
+def yieldMatchingTense(gv,tense):
+    v,dat = gv
+    for y in dat[1]:
+        if y[2][len(dat[0][1])+13:-1] == tense:
+            yield (y[0],y[3:])
+
+def yieldContainsFull(gv):
+    v,dat = gv
+    for y in dat[1]:
+        if v in y[0]:
+            yield (y[0],y[3:])
+
+def countContainsFull(gv):
+    return len(zip(range(len(gv[1][1])),yieldContainsFull(gv)))
+
 # ===============================================
 
 if __name__ == '__main__':
